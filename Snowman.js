18bc5983826e2a2b15a8e8e6157732ec6ce9ba78
s171082 <s171082@grandviewc4.net>
2016-09-03 14:27:36 -0500

    Create Snowman.js

---
+++ Snowman.js
@@ -0,0 +1,43 @@
+background(174, 231, 235);
+fill(247, 255, 0);
+ellipse(48,41,150,150);
+fill(204, 202, 204);
+rect(0,334,399,65);
+fill(245, 245, 245);
+// simple snowman
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+line(88, 192, 156, 198);
+line(319, 197, 245, 201);
+fill(255, 119, 0);
+triangle(199, 117, 258, 127, 198, 128);
+// draw a point of size 3
+strokeWeight(8);
+point(211, 108);
+point(184,108);
+strokeWeight(7);
+point(200,200);
+point(200,220);
+point(200, 180);
+point(200,270);
+point(200,290);
+point(200,310);
+point(200,330);
+strokeWeight(4);
+point(179,131);
+point(188,135);
+point(199,138);
+point(210,136);
+point(221,132);
+strokeWeight(1);
+// draw a rounded rectangle at (150, 300) with width 100 and height 50
+fill(66, 54, 54);
+rect(163, 26, 68, 62, 11);
+// draw a rectangle at (100, 50) with width 200 and height 100
+rect(154, 83, 88, 13);
+
+quad(316, 153, 347, 155, 380, 82, 307, 76);
+strokeWeight(4);
+line(333, 154, 312, 350);
+
